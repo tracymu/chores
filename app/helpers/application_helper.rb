239ea52443fe53f707e68d86f29fd1d3cda52b50
tracymu <tracy.musung@moomumedia.com>
2016-01-08 18:38:45 +1100

    Help the view return some data about each chore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
   def background_color(chore)
-    byebug
+    @chore = chore
+    case 
+    when days_passed <  chore.frequency
+      'green'
+    when days_passed <= (chore.frequency * 1.25 )
+      'orange'
+    else
+      'red'
+    end
+  end
+  
+  def days_since_done(chore)
+    days_passed.to_i
+  end
+  
+  def days_passed
+    Date.today - @chore.last_done
   end
 end
